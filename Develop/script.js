b37e0f6f22adb4a9c912765a70e92bddbe7d20f8
Shawn Davis <smd031@gmail.com>
2023-06-29 17:43:30 -0500

    button now works

--- Develop/script.js
+++ Develop/script.js
@@ -31,74 +31,77 @@ function shuffle(array) {
 
 
 // My Code
+function generatePassword() {
+  // all variables used
+  var newPassTemp = [];
+  var newPassFinal = [];
+  var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M",'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  var numbers = ['0','1','2','3','4','5','6','7','8','9'];
+  var specialCharacters = ["!","\"","#","$","%","&","\'","(",")","*","+","\,","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
 
-// all variables used
-var newPassTemp = [];
-var newPassFinal = [];
-var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M",'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var numbers = ['0','1','2','3','4','5','6','7','8','9'];
-var specialCharacters = ["!","\"","#","$","%","&","\'","(",")","*","+","\,","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
+  // Pop-up code
+  var howManyCharsNeededStr = window.prompt("How many characters long should the password be? Anywhere from 8-128 is allowed")
+  var yesLower = window.confirm("Would you like to have lowercase letters?");
+  var yesUpper = window.confirm("Would you like to have uppercase letters?");
+  var yesNumbers = window.confirm("Would you like to have numbers?");
+  var yesSpecial = window.confirm("Would you like to have special characters?");
 
-// Pop-up code
-var howManyCharsNeededStr = window.prompt("How many characters long should the password be? Anywhere from 8-128 is allowed")
-var yesLower = window.confirm("Would you like to have lowercase letters?");
-var yesUpper = window.confirm("Would you like to have uppercase letters?");
-var yesNumbers = window.confirm("Would you like to have numbers?");
-var yesSpecial = window.confirm("Would you like to have special characters?");
+  // Edit this in pop up windows
+  var howManyCharsNeeded = parseInt(howManyCharsNeededStr);
+  var howManyCharsNeededNow = howManyCharsNeeded;
 
-// Edit this in pop up windows
-var howManyCharsNeeded = parseInt(howManyCharsNeededStr);
-var howManyCharsNeededNow = howManyCharsNeeded;
+  // Check if at least 8 characters and at max 128 characters
+  if (howManyCharsNeeded <= 8 || howManyCharsNeeded >= 128) {
+    alert("Please have a number at least 8 and at max 128");
+  }
 
-// Check if at least 8 characters and at max 128 characters
-if (howManyCharsNeeded <= 8 || howManyCharsNeeded >= 128) {
-  alert("Please have a number at least 8 and at max 128");
+  // Check if using lowercase and adding one to password if true
+  if (yesLower == true) {
+    const random = Math.floor(Math.random() * lowercaseLetters.length);
+    newPassFinal = newPassFinal.concat(lowercaseLetters[random]);
+    newPassTemp = newPassTemp.concat(lowercaseLetters);
+    howManyCharsNeededNow = howManyCharsNeededNow-1;
+    console.log("password will contain lowercase letters");
+  }
+
+  // Check if using uppercase and adding one to password if true
+  if (yesUpper == true) {
+    const random = Math.floor(Math.random() * uppercaseLetters.length);
+    newPassFinal = newPassFinal.concat(uppercaseLetters[random]);
+    newPassTemp = newPassTemp.concat(uppercaseLetters);
+    howManyCharsNeededNow = howManyCharsNeededNow-1;
+    console.log("password will contain uppercase letters");
+  }
+
+  // Check if using numbers and adding one to password if true
+  if (yesNumbers == true) {
+    const random = Math.floor(Math.random() * numbers.length);
+    newPassFinal = newPassFinal.concat(numbers[random]);
+    newPassTemp = newPassTemp.concat(numbers);
+    howManyCharsNeededNow = howManyCharsNeededNow-1;
+    console.log("password will contain numbers")
+  }
+
+  // Check if using special characters and adding one to password if true
+  if (yesSpecial == true) {
+    const random = Math.floor(Math.random() * specialCharacters.length);
+    newPassFinal = newPassFinal.concat(specialCharacters[random])
+    newPassTemp = newPassTemp.concat(specialCharacters);
+    howManyCharsNeededNow = howManyCharsNeededNow-1;
+    console.log("password will contain special characters");
+  }
+
+  // Check how many more characters we need and filling in with allowed characters
+  for (var i = 0; i < howManyCharsNeededNow; i++) { 
+    const random = Math.floor(Math.random() * newPassTemp.length);
+    newPassFinal = newPassFinal.concat(newPassTemp[random]);
+  }
+
+  // Shuffles the characters being used in the password and joins them
+  var shuffledNewPassFinal = shuffle(newPassFinal);
+  console.log("My new password is : " + shuffledNewPassFinal.join(''));
 }
 
-// Check if using lowercase and adding one to password if true
-if (yesLower == true) {
-  const random = Math.floor(Math.random() * lowercaseLetters.length);
-  newPassFinal = newPassFinal.concat(lowercaseLetters[random]);
-  newPassTemp = newPassTemp.concat(lowercaseLetters);
-  howManyCharsNeededNow = howManyCharsNeededNow-1;
-  console.log("password will contain lowercase letters");
-}
 
-// Check if using uppercase and adding one to password if true
-if (yesUpper == true) {
-  const random = Math.floor(Math.random() * uppercaseLetters.length);
-  newPassFinal = newPassFinal.concat(uppercaseLetters[random]);
-  newPassTemp = newPassTemp.concat(uppercaseLetters);
-  howManyCharsNeededNow = howManyCharsNeededNow-1;
-  console.log("password will contain uppercase letters");
-}
-
-// Check if using numbers and adding one to password if true
-if (yesNumbers == true) {
-  const random = Math.floor(Math.random() * numbers.length);
-  newPassFinal = newPassFinal.concat(numbers[random]);
-  newPassTemp = newPassTemp.concat(numbers);
-  howManyCharsNeededNow = howManyCharsNeededNow-1;
-  console.log("password will contain numbers")
-}
-
-// Check if using special characters and adding one to password if true
-if (yesSpecial == true) {
-  const random = Math.floor(Math.random() * specialCharacters.length);
-  newPassFinal = newPassFinal.concat(specialCharacters[random])
-  newPassTemp = newPassTemp.concat(specialCharacters);
-  howManyCharsNeededNow = howManyCharsNeededNow-1;
-  console.log("password will contain special characters");
-}
-
-// Check how many more characters we need and filling in with allowed characters
-for (var i = 0; i < howManyCharsNeededNow; i++) { 
-  const random = Math.floor(Math.random() * newPassTemp.length);
-  newPassFinal = newPassFinal.concat(newPassTemp[random]);
-}
-
-// Shuffles the characters being used in the password and joins them
-var shuffledNewPassFinal = shuffle(newPassFinal);
-console.log("My new password is : " + shuffledNewPassFinal.join(''));
 
